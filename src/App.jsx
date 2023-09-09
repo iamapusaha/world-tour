36b9ff059da48096e59b1778f4bef66712c04bd2
iamapusaha <apusaha392@gmail.com>
2023-09-09 21:44:49 +0600

    fetch and load data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Countries from './components/Countries/Countries'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
 
       <h1>Vite + React</h1>
+      <Countries></Countries>
 
     </>
   )
