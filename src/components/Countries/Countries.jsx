fd3f4b328000d1eecbc39593a6dbba98201a3472
iamapusaha <apusaha392@gmail.com>
2023-09-10 00:30:34 +0600

    display more data & button

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
+import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([])
@@ -12,12 +13,14 @@ const Countries = () => {
     return (
         <div>
             <h1>Total number o countries: {countries.length}</h1>\
-            {
-                countries.map(country => <Country
-                    country={country}
-                    key={country.ccn3}
-                ></Country>)
-            }
+            <div className="countries-container">
+                {
+                    countries.map(country => <Country
+                        country={country}
+                        key={country.cca3}
+                    ></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,21 @@
-/* eslint-disable react/prop-types */
+
+import { useState } from 'react';
 import './Country.css'
 const Country = ({ country }) => {
-    const { name, flags } = country;
-    console.log(country);
+    const [visited, setVisited] = useState(false)
+    const handleVisited = () => {
+        setVisited(true)
+    }
+    const { name, flags, population, area, cca3 } = country;
+    // console.log(country);
     return (
         <div className='country'>
-            <h3>name:{name.common}</h3>
-            <img src={flags.png} alt="" />
+            <h3>Name:{name.common}</h3>
+            <img className='img-width' src={flags.png} alt="" />
+            <h5>Population: {population}</h5>
+            <h6>Area: {area}</h6>
+            <p><small>code: {cca3}</small></p>
+            <button onClick={handleVisited}>{visited ? 'visited' : 'going'}</button>
         </div>
     );
 };
